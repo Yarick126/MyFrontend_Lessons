f3996e4b7615908d3e95baa2e09e0dd213205fdb
Yarick126 <gadzhikurbanovy@mail.ru>
2023-03-06 18:07:11 +0300

    TwoMaket

---
+++ Главна,оставить заявку/Подобрать_персонал/script.js
@@ -0,0 +1,30 @@
+let btn = document.querySelector('#btn_1');
+let f = document.getElementById('form_1');
+
+let btn_2 = document.querySelector('#btn_2');
+let f_2 = document.getElementById('form_2');
+
+btn.addEventListener('click', function(){
+  if(btn.getAttribute('value')== 'close'){
+    f.style.display = 'flex';
+    btn.setAttribute('value','open');
+  }
+  else{
+    f.style.display = 'none';
+    btn.setAttribute('value','close');
+  }
+})
+
+btn_2.addEventListener('click', function(){
+  if(btn_2.getAttribute('value')== 'close'){
+    f_2.style.display = 'flex';
+    btn_2.setAttribute('value','open');
+  }
+  else{
+    f_2.style.display = 'none';
+    btn_2.setAttribute('value','close');
+  }
+})
+
+document.getElementsByClassName('onlyNum').onkeydown = function (e) {
+  return !(/^[А-Яа-яA-Za-z ]$/.test(e.key));}
